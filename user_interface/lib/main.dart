d3729ab027c9b860a74dc77d77af5e343467fafb
Ruth Wangare AideenHah-yil <wangarenjorogeruth@gmail.com>
2024-05-24 11:46:23 +0300

    working on the main dart file adding the splashScreen logo

---
+++ user_interface/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('images/samurai.png',
+        width: 100,
+        height: 100,
+        ),
+      ),
+    );
+  }
+}
